25c456a9ca80045fe49670fe8048e811c92e39ca
Christoph Herbst <chris.p.herbst@gmail.com>
2017-07-07 14:24:31 +0200

    load textdomain on plugins_loaded trigger
    this is how it is documented in the codex:
    https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
    
    before, the translation files were sometimes loded before the
    current locale was correctly set, leading to missing translations

--- badgeos-community.php
+++ badgeos-community.php
@@ -36,13 +36,11 @@ class BadgeOS_Community {
 		$this->directory_path = plugin_dir_path( __FILE__ );
 		$this->directory_url  =  trailingslashit ( plugins_url ( '', __FILE__ ) );
 
-		// Load translations
-		load_plugin_textdomain( 'badgeos-community', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-
 		// Run our activation
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 
 		// If BadgeOS is unavailable, deactivate our plugin
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
 		add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
 		add_action( 'plugins_loaded', array( $this, 'includes' ) );
 		add_action( 'bp_include', array( $this, 'bp_include' ) );
@@ -97,6 +95,14 @@ class BadgeOS_Community {
         
         wp_enqueue_style( 'bos-community-style', $this->directory_url . 'css/bos-community.css', [] );
     }
+
+    /**
+	 * Load translations.
+	 */
+	function load_textdomain() {
+		// Load translations
+		load_plugin_textdomain( 'badgeos-community', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
 
 	/**
 	 * Files to include for BadgeOS integration.
